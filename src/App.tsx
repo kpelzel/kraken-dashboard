b6547425f833a42a3388a3c5d54ee157b7a4cf1f
Kevin Pelzel <kevinpelzel22@gmail.com>
2020-08-24 14:20:44 -0600

    reworked handling of websocket message

--- src/App.tsx
+++ src/App.tsx
@@ -342,107 +342,110 @@ class App extends Component<AppProps, AppState> {
       })
   }
 
-  handleWebSocketMessage = (jsonData: any) => {
-    const newNodes = cloneDeep(this.state.nodes)
-    const newDscNodes = cloneDeep(this.state.dscNodes)
-    let dscUpdateHappened = false
-    for (let i = 0; i < jsonData.length; i++) {
-      if (jsonData[i].type === 1) {
-        // If it's a creation message, stop the loop and pull cfgNodes and dscNodes
-        if (jsonData[i].data.includes('(CREATE)') || jsonData[i].data.includes('(CFG_UPDATE')) {
-          console.log('Creation or update found. Close websocket and pull dsc and cfg nodes')
-          this.setState({
-            liveConnectionActive: 'REFETCH',
-          })
-          break
-        } else {
-          const jsonMessage: WsMessage = jsonData[i]
-          // This is a physstate or runstate update
-          if (jsonMessage.url === '/PhysState' || jsonMessage.url === '/RunState') {
-            const base64Id = uuidToBase64(jsonMessage.nodeid)
-            const newNode = newNodes.get(base64Id)
-            const newDscNode = newDscNodes.get(base64Id)
-            if (newNode === undefined || newDscNode === undefined) {
-              console.log("couldn't find node. Closing websocket and pulling dsc and cfg nodes")
-              this.setState({
-                liveConnectionActive: 'REFETCH',
-              })
+  handleWebSocketMessage = (jsonData: WsMessage) => {
+    console.log('got websocket message: ', jsonData)
+    if (jsonData.type === 1) {
+      // If it's a creation message, stop the loop and pull cfgNodes and dscNodes
+      if (jsonData.data.includes('(CREATE)') || jsonData.data.includes('(CFG_UPDATE')) {
+        console.log('Creation or update found. Close websocket and pull dsc and cfg nodes')
+        this.setState({
+          liveConnectionActive: 'REFETCH',
+        })
+        return
+      } else {
+        const jsonMessage: WsMessage = jsonData
+        // This is a physstate or runstate update
+        if (jsonMessage.url === '/PhysState' || jsonMessage.url === '/RunState') {
+          const newNodes = cloneDeep(this.state.nodes)
+          const newDscNodes = cloneDeep(this.state.dscNodes)
+          const base64Id = uuidToBase64(jsonMessage.nodeid)
+          const newNode = newNodes.get(base64Id)
+          const newDscNode = newDscNodes.get(base64Id)
+          if (newNode === undefined || newDscNode === undefined) {
+            console.log("couldn't find node. Closing websocket and pulling dsc and cfg nodes")
+            this.setState({
+              liveConnectionActive: 'REFETCH',
+            })
+            return
+          }
+          switch (jsonMessage.url) {
+            case '/PhysState':
+              newNode.physState = jsonMessage.value as KrakenPhysState
+              newDscNode.physState = jsonMessage.value as KrakenPhysState
+              if (jsonMessage.value === 'POWER_OFF') {
+                newNode.runState = 'UNKNOWN'
+                newDscNode.runState = 'UNKNOWN'
+              } else if (jsonMessage.value === 'PHYS_HANG') {
+                newNode.runState = 'UNKNOWN'
+                newDscNode.runState = 'UNKNOWN'
+              }
               break
-            }
-            switch (jsonMessage.url) {
-              case '/PhysState':
-                newNode.physState = jsonMessage.value as KrakenPhysState
-                newDscNode.physState = jsonMessage.value as KrakenPhysState
-                if (jsonMessage.value === 'POWER_OFF') {
-                  newNode.runState = 'UNKNOWN'
-                  newDscNode.runState = 'UNKNOWN'
-                } else if (jsonMessage.value === 'PHYS_HANG') {
-                  newNode.runState = 'UNKNOWN'
-                  newDscNode.runState = 'UNKNOWN'
-                }
+            case '/RunState':
+              if (
+                jsonMessage.value !== 'UNKNOWN' &&
+                (newNode.physState === 'PHYS_UNKNOWN' ||
+                  newNode.physState === 'POWER_OFF' ||
+                  newNode.physState === 'PHYS_HANG')
+              ) {
+                console.log(
+                  "Tried to change node's runstate while phystate is unknown. Closing websocket and pulling dsc and cfg nodes"
+                )
+                this.setState({
+                  liveConnectionActive: 'REFETCH',
+                })
+                return
+              } else {
+                newNode.runState = jsonMessage.value as KrakenRunState
+                newDscNode.runState = jsonMessage.value as KrakenRunState
                 // newNodes.set(base64Id, newNode)
                 break
-              case '/RunState':
-                if (
-                  jsonMessage.value !== 'UNKNOWN' &&
-                  (newNode.physState === 'PHYS_UNKNOWN' ||
-                    newNode.physState === 'POWER_OFF' ||
-                    newNode.physState === 'PHYS_HANG')
-                ) {
-                  console.log(
-                    "Tried to change node's runstate while phystate is unknown. Closing websocket and pulling dsc and cfg nodes"
-                  )
-                  this.setState({
-                    liveConnectionActive: 'REFETCH',
-                  })
-                  break
-                } else {
-                  newNode.runState = jsonMessage.value as KrakenRunState
-                  newDscNode.runState = jsonMessage.value as KrakenRunState
-                  // newNodes.set(base64Id, newNode)
-                  break
-                }
-              default:
-                break
-            }
-            dscUpdateHappened = true
-          } else if (jsonMessage.url.includes('type.googleapis.com')) {
-            // This is an extensions update
-            const base64Id = uuidToBase64(jsonMessage.nodeid)
-            let newNode = newNodes.get(base64Id)
-            let newDscNode = newDscNodes.get(base64Id)
-            if (newNode === undefined || newDscNode === undefined) {
-              console.log("couldn't find node. Closing websocket and pulling dsc and cfg nodes")
-              this.setState({
-                liveConnectionActive: 'REFETCH',
-              })
+              }
+            default:
               break
-            }
-            const updatedNode = updateFromWsMessage(newNode, jsonMessage)
-            if (updatedNode !== undefined) {
-              newNode = updatedNode
-              dscUpdateHappened = true
-            }
-            const updatedDscNode = updateFromWsMessage(newDscNode, jsonMessage)
-            if (updatedDscNode !== undefined) {
-              newDscNode = updatedDscNode
-              dscUpdateHappened = true
-            }
+          }
+          this.setState({
+            nodes: newNodes,
+            dscNodes: newDscNodes,
+          })
+        } else if (jsonMessage.url.includes('type.googleapis.com')) {
+          // This is an extensions update
+          const newNodes = cloneDeep(this.state.nodes)
+          const newDscNodes = cloneDeep(this.state.dscNodes)
+          const base64Id = uuidToBase64(jsonMessage.nodeid)
+          let newNode = newNodes.get(base64Id)
+          let newDscNode = newDscNodes.get(base64Id)
+          if (newNode === undefined || newDscNode === undefined) {
+            console.log("couldn't find node. Closing websocket and pulling dsc and cfg nodes")
+            this.setState({
+              liveConnectionActive: 'REFETCH',
+            })
+            return
+          }
+          let dscUpdateHappened = false
+          const updatedNode = updateFromWsMessage(newNode, jsonMessage)
+          if (updatedNode !== undefined) {
+            newNode = updatedNode
+            dscUpdateHappened = true
+          }
+          const updatedDscNode = updateFromWsMessage(newDscNode, jsonMessage)
+          if (updatedDscNode !== undefined) {
+            newDscNode = updatedDscNode
+            dscUpdateHappened = true
+          }
+          if (dscUpdateHappened) {
+            this.setState({
+              nodes: newNodes,
+              dscNodes: newDscNodes,
+            })
           }
         }
-      } else if (
-        this.state.updatingGraph !== undefined &&
-        (jsonData[i].type === 2 || jsonData[i].type === 5) &&
-        jsonData[i].nodeid === base64ToUuid(this.state.updatingGraph).toLowerCase()
-      ) {
-        this.getGraph(this.state.updatingGraph)
       }
-    }
-    if (dscUpdateHappened) {
-      this.setState({
-        nodes: newNodes,
-        dscNodes: newDscNodes,
-      })
+    } else if (
+      this.state.updatingGraph !== undefined &&
+      (jsonData.type === 2 || jsonData.type === 5) &&
+      jsonData.nodeid === base64ToUuid(this.state.updatingGraph).toLowerCase()
+    ) {
+      this.getGraph(this.state.updatingGraph)
     }
   }
 
